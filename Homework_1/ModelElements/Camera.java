e4b1f73a6aeb354059f145479eb924082a7f916d
Olga Kutueva <kutueva.olga@gmail.com>
2024-01-26 21:01:33 +0200

    Homework_1, класс Camera и зависимости

---
+++ Homework_1/ModelElements/Camera.java
@@ -0,0 +1,13 @@
+package ModelElements;
+
+public class Camera {
+    public Point3D Location;
+    public Angle3D Angel;
+
+    public void Rotate(Angle3D Angel){
+
+    }
+    public void Move(Point3D Location){
+        
+    }
+}
