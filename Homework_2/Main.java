44de5ccf4bb98f86aabb56912d007390aebe0feb
Olga Kutueva <kutueva.olga@gmail.com>
2024-01-27 17:27:45 +0200

    Homework 2, генератор вынесен в отдельный метод

--- Homework_2/Main.java
+++ Homework_2/Main.java
@@ -6,12 +6,16 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     public static void main(String[] args){
-        Random rnd = ThreadLocalRandom.current();
         List<GenItem> fabricList = new ArrayList<>();
 
         fabricList.add(new GenGoldReward());
         fabricList.add(new GenGemReward());
 
+        createAndOpenReward(fabricList);
+    }
+
+    static void createAndOpenReward(List<GenItem> fabricList){
+        Random rnd = ThreadLocalRandom.current();
         for(int i=0; i<20; i++) {
             IGameItem fabric = fabricList.get(Math.abs(rnd.nextInt() % fabricList.size())).createItem();
             fabric.open();
