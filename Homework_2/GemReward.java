cc9f48a85b4b820ff37a0f1857cfbf111d425aff
Olga Kutueva <kutueva.olga@gmail.com>
2024-01-27 17:23:10 +0200

    Homework 2, сундук с изумрудами

---
+++ Homework_2/GemReward.java
@@ -0,0 +1,7 @@
+public class GemReward implements IGameItem{
+    
+    @Override
+    public void open() {
+        System.out.println("GemItem is opened!");
+    }
+}
---
+++ Homework_2/GenGemReward.java
@@ -0,0 +1,7 @@
+public class GenGemReward implements GenItem {
+
+    @Override
+    public IGameItem createItem {
+        return new GemReward();
+    }
+}
