1d6d8d87741bc24415a2ce255f4753fe57478a1b
Olga Kutueva <kutueva.olga@gmail.com>
2024-02-09 19:46:59 +0200

    Homework 4. Добавлен класс Bank

---
+++ Homework_4/Bank.java
@@ -0,0 +1,14 @@
+package Homework4;
+
+public class Bank {
+    private long card;
+
+    // операция проведения платежа и статус её успешности
+    // итоговая сумма платежа может отличаться от стоимости билета, если есть комиссия
+    public boolean getPayment(int total) {
+    }
+
+    // операция возврата денег и её статус
+    public boolean getRefund(int total){
+    }
+}
